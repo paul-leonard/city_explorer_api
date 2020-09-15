534c904d6ca39b2435dc854298bad35dbda0da6d
paul-leonard <paul.ryan.leonard@gmail.com>
2020-09-14 22:52:31 -0700

    weather and date returning

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ require('dotenv').config();
 // Application Dependencies
 const express = require('express');
 const cors = require('cors');
-const { response } = require('express');
+// const { response } = require('express');
 
 //Application Setup
 const PORT = process.env.PORT;
@@ -16,17 +16,14 @@ app.use(cors());
 // not sure why i put this in here, maybe from day 5 notes
 // const { response } = require('express');
 
-
 //Basic test of home page
 app.get('/', (request, response) => {
-  console.log('response body:', response);
-  console.log('request method', request);
   response.send('hello world to explore');
 })
 
 // API Routes
-app.get('/location', handleLocation);
-app.get('/weather', handleWeather);
+app.get(`/location`, handleLocation);
+app.get(`/weather`, handleWeather);
 app.get('*', notFoundHandler);
 
 // Helper Functions
@@ -48,13 +45,13 @@ function City(city,locationData) {
   this.formatted_query = locationData[0].display_name;
   this.latitude = locationData[0].lat;
   this.longitude = locationData[0].lon;
-}
+};
 
 function handleWeather(request, response) {
   try {
     const weatherData = require('./data/weather.json');
     const city = request.query.city;
-    const cityForecast = eightDayForecast(city, weatherData);
+    const cityForecast = multiDayForecast(city, weatherData);
     response.send(cityForecast);
   }
   catch(error) {
@@ -63,27 +60,25 @@ function handleWeather(request, response) {
   }
 }
 
-function eightDayForecast(city,weatherData) {
-  let eightForecastOutput = [];
-  for (let i = 0; i < 8; i++) {
-    eightForecastOutput.push(new Forecast(city,weatherData,i));
+function multiDayForecast(city,weatherData) {
+  let multiForecastOutput = [];
+  for (let i = 0; i < weatherData.data.length; i++) {
+    multiForecastOutput.push(new Forecast(city,weatherData,i));
   }
-  return eightForecastOutput;
+  return multiForecastOutput;
 }
 
 function Forecast(city,weatherData,day) {
   this.forecast = weatherData.data[day].weather.description;
-  this.time = weatherData.data[day].valid-Date;
-
-  // "forecast": "Mostly cloudy in the morning.",
-  // "time": "Tue Jan 02 2001"
+  let dateString = weatherData.data[day].datetime;
+  let tempDateObject = new Date(dateString);
+  this.time = tempDateObject.toDateString();
 }
-
 
 function notFoundHandler(request, response) {
   response.status(404).send('that page does not exist. should we make it?')
 }
 
 app.listen(PORT, ()=> {
-  console.log('listening on port: ${PORT}');
+  console.log(`listening on port: ${PORT}`);
   });
