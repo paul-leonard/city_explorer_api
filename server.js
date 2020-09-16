61fafbefb66b173e90c6803fcc7536a7d7ef0f63
paul-leonard <paul.ryan.leonard@gmail.com>
2020-09-15 18:12:23 -0700

    location api plugged in

--- server.js
+++ server.js
@@ -3,18 +3,17 @@
 // Load Environment Variables from the .env file
 require('dotenv').config();
 
-// Application Dependencies
+// Require Application Dependencies - 3rd party from npm
 const express = require('express');
 const cors = require('cors');
-// const { response } = require('express');
+const superagent = require('superagent');
 
-//Application Setup
+//Application Setup - assign file constants
 const PORT = process.env.PORT;
 const app = express();
-app.use(cors());
 
-// not sure why i put this in here, maybe from day 5 notes
-// const { response } = require('express');
+// set up middleware - for global thigns happening in your app
+app.use(cors());
 
 //Basic test of home page
 app.get('/', (request, response) => {
@@ -28,23 +27,36 @@ app.get('*', notFoundHandler);
 
 // Helper Functions
 function handleLocation(request, response) {
-  try {
-    const locationData = require('./data/location.json');
-    const city = request.query.city;
-    const cityData = new City(city, locationData);
-    response.send(cityData);
-  }
-  catch (error) {
-    console.log('ERROR',error);
-    response.status(500).send('Sorry, something went wrong');
-  } 
+  // try {
+    let city = request.query.city;
+    let key = process.env.GEOCODE_API_KEY;
+
+    let url = `https://us1.locationiq.com/v1/search.php?key=${key}&q=${city}&limit=1&format=json`
+
+    superagent.get(url)
+      .then(incomingLocationData => {
+        let locationData = incomingLocationData.body[0];
+        const cityData = new City(city, locationData);
+        response.send(cityData);
+      })
+      .catch( (error) => {
+        response.status(500).send('Sorry, something went wrong');
+      });
 }
 
 function City(city,locationData) {
   this.search_query = city;
-  this.formatted_query = locationData[0].display_name;
-  this.latitude = locationData[0].lat;
-  this.longitude = locationData[0].lon;
+  this.formatted_query = locationData.display_name;
+  this.latitude = locationData.lat;
+  this.longitude = locationData.lon;
+
+  // example response format:
+  // {
+  //   "search_query": "seattle",
+  //   "formatted_query": "Seattle, WA, USA",
+  //   "latitude": "47.606210",
+  //   "longitude": "-122.332071"
+  // }
 };
 
 function handleWeather(request, response) {
